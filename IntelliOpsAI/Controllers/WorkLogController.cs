366ac4d8d14b3586b008e726dac7b9e4a21ae2cc
Parina Jain <parinajainindore@gmail.com>
2026-05-13 14:11:12 +0530

    Integrated WorkLog module with dashboard navigation and UI flow

---
+++ IntelliOpsAI/Controllers/WorkLogController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using IntelliOpsAI.Models;
+using System.Collections.Generic;
+
+namespace IntelliOpsAI.Controllers
+{
+    public class WorkLogController : Controller
+    {
+        private static List<WorkLog> logs = new List<WorkLog>();
+
+        public IActionResult Index()
+        {
+            return View(logs);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(WorkLog log)
+        {
+            log.Id = logs.Count + 1;
+            log.Date = DateTime.Now;
+
+            logs.Add(log);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ IntelliOpsAI/Models/WorkLog.cs
@@ -0,0 +1,17 @@
+﻿namespace IntelliOpsAI.Models
+{
+    public class WorkLog
+    {
+        public int Id { get; set; }
+
+        public string EmployeeName { get; set; }
+
+        public string TaskName { get; set; }
+
+        public int HoursWorked { get; set; }
+
+        public string Status { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
