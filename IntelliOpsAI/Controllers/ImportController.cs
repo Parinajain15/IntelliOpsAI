4c1200b07bdcf8810ff0e829802ee83a1819041b
Parina Jain <parinajainindore@gmail.com>
2026-05-14 13:40:29 +0530

    Added CSV-based data import module and enhanced navigation for enterprise analytics workflow

---
+++ IntelliOpsAI/Controllers/ImportController.cs
@@ -0,0 +1,47 @@
+﻿using CsvHelper;
+using IntelliOpsAI.Data;
+using IntelliOpsAI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+
+namespace IntelliOpsAI.Controllers
+{
+    public class ImportController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ImportController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Upload(IFormFile file)
+        {
+            if (file != null && file.Length > 0)
+            {
+                using (var reader = new StreamReader(file.OpenReadStream()))
+                using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    var records = csv.GetRecords<WorkLog>().ToList();
+
+                    foreach (var record in records)
+                    {
+                        _context.WorkLogs.Add(record);
+                    }
+
+                    _context.SaveChanges();
+                }
+
+                ViewBag.Message = "CSV imported successfully!";
+            }
+
+            return View("Index");
+        }
+    }
+}
